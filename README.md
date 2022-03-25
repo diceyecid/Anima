b6fda5b569b4bcf25d2e2f3b0a88cf440fcdb570
Ross <49296963+diceyecid@users.noreply.github.com>
2022-03-25 00:47:27 -0700

    added object detection codes

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Anima
+
 An object-aware cartoonization method
+
+## Installation
+
+1. Clone this repository
+
+2. Run setup from the repository root directory
+
+   ```bash
+   bash setup.sh
+   ```
+
+3. Start Python virtual environment
+
+   ```bash
+   source ./venv/bin/activate
+   ```
+
+4. Download pre-trained weights inside the root directory from [here](https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5)
---
+++ detect.py
@@ -0,0 +1,84 @@
+import cv2
+import os
+import sys
+import mrcnn.config as mConfig
+import mrcnn.model as mModel
+import mrcnn.visualize as mVisualize
+
+# COCO class labels 
+CLASS_NAMES = [ 
+        'BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 
+        'bus', 'train', 'truck', 'boat', 'traffic light',
+        'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+        'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+        'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+        'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+        'kite', 'baseball bat', 'baseball glove', 'skateboard',
+        'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+        'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+        'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+        'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+        'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+        'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+        'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+        'teddy bear', 'hair drier', 'toothbrush' 
+        ]
+
+# custom configuration
+class InferenceConfig( mConfig.Config ):
+    NAME = 'coco_inference'
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    NUM_CLASSES = len( CLASS_NAMES )
+
+# main execution
+def main( image ):
+    # initilize configuration and display it
+    config = InferenceConfig()
+
+    # initialize Mask R-CNN model for inference
+    model = mModel.MaskRCNN( mode = 'inference', config = config, model_dir = os.getcwd() )
+
+    # load weight to model
+    model.load_weights( filepath = 'mask_rcnn_coco.h5', by_name = True )
+
+    # forward pass
+    res = model.detect( [ image ], verbose = 0 )
+    r = res[0]
+
+    # visualize results
+    colHead = '%      class      at [ y1  x1  y2  x2]'
+    print( colHead )
+    print( '-' * len( colHead ) )
+    for s, c, b in zip( r['scores'], r['class_ids'], r['rois'] ):
+        print( f'{s*100:.2f}% {CLASS_NAMES[ c ]:10} at {b}' )
+
+    mVisualize.display_instances( 
+            image = image, 
+            boxes = r['rois'], 
+            masks = r['masks'],
+            class_ids = r['class_ids'],
+            class_names = CLASS_NAMES,
+            scores = r['scores'] 
+            )
+
+# call enterance
+if __name__ == '__main__':
+    # if image is not supplied, read default image
+    if len( sys.argv ) < 2:
+        image = cv2.imread( './samples/default.jpg' )
+
+    # otherwise read supplied image
+    else:
+        image = cv2.imread( sys.argv[1] )
+
+        # if supplied image does not exist, print error and finish execution
+        if image is None:
+            print( 'Image does not exist in samples folder' )
+            sys.exit( 1 )
+
+    # convert image to RGB channel
+    image = cv2.cvtColor( image, cv2.COLOR_BGR2RGB )
+    main( image )
+
+
