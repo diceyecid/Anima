c22cd57ec81fb67845542eb69b6fd9c7fda8db0e
A <laz>
2022-04-11 23:35:00 -0700

    add comments

--- UIControl.py
+++ UIControl.py
@@ -1,4 +1,4 @@
-import os, output
+import os
 import wx
 from wx.lib.pubsub import pub
 import wx.lib.statbmp as SB
@@ -6,10 +6,10 @@ import wx.lib.statbmp as SB
 from PIL import Image
 
 PhotoMaxSize = 800
+maxSize = 600
 pub.sendMessage("state", message = 'input')
-
+# drop module from https://www.blog.pythonlibrary.org/2017/10/25/wx_drag_and_drop_images/
 class DropTarget(wx.FileDropTarget):
-
     def __init__(self, widget):
         wx.FileDropTarget.__init__(self)
         self.widget = widget
@@ -22,8 +22,9 @@ class DropTarget(wx.FileDropTarget):
         return True
     
 
-
+#Input frame
 class Input(wx.Frame):
+    #create input frame and set background color
     def __init__(self):
         self.frame = wx.Frame(None, title='Anima', size = (1200, 600))
 
@@ -31,10 +32,13 @@ class Input(wx.Frame):
         self.panel.SetBackgroundColour(wx.Colour(233,243,255,255))
         pub.subscribe(self.update_image_on_dnd, 'dnd')
         self.createWidgets()
-        self.frame.Show()
+        self.frame.Show(), 
 
+    # create components for the input frame
     def createWidgets(self):
+
         instructions = 'Browse for an image or Drag and Drop'
+        #create placeholder to let the user drop their image -- https://www.blog.pythonlibrary.org/2017/10/25/wx_drag_and_drop_images/
         img = wx.Image(800,600)
 
         self.imageCtrl = SB.GenStaticBitmap(self.panel, wx.ID_ANY, 
@@ -42,6 +46,7 @@ class Input(wx.Frame):
         filedroptarget = DropTarget(self)
         self.imageCtrl.SetDropTarget(filedroptarget)
 
+        #Set title and font styles
         title = "Anima" 
         lbl = wx.StaticText(self.panel,-1,style = wx.ALIGN_CENTER) 
         font = wx.Font(48, wx.FONTFAMILY_MODERN, wx.NORMAL, wx.NORMAL)
@@ -52,20 +57,23 @@ class Input(wx.Frame):
         instructLbl = wx.StaticText(self.panel, label=instructions)
         instructLbl.SetFont(subFont)
         self.style = 'shinkai'
+
+        #Create drop-down menu
         choices = ['shinkai', 'hayao', 'hosoda', 'paprika'] 
         self.choice = wx.Choice(self.panel,choices = choices)
         self.choice.Bind(wx.EVT_CHOICE, self.onChoice)
 
-
+        #Create button to let the user browse images from local disk -- https://www.blog.pythonlibrary.org/2017/10/25/wx_drag_and_drop_images/
         self.photoTxt = wx.TextCtrl(self.panel, size=(300,-1))
         browseBtn = wx.Button(self.panel, label='Browse')
         browseBtn.Bind(wx.EVT_BUTTON, self.on_browse)
 
+        #create run button
         runButton = wx.Button(self.panel, label='Run')
         runButton.Bind(wx.EVT_BUTTON, self.onRun)
         
         
-
+        #use sizer to organize the layout
         self.mainSizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer = wx.BoxSizer(wx.HORIZONTAL)
 
@@ -89,9 +97,7 @@ class Input(wx.Frame):
         self.panel.Layout()
 
     def on_browse(self, event):
-        """ 
-        Browse for file
-        """
+        # browse image from local disk -- https://www.blog.pythonlibrary.org/2017/10/25/wx_drag_and_drop_images/
         wildcard = "JPEG files (*.jpg)|*.jpg"
         dialog = wx.FileDialog(None, "Choose a file",
                                wildcard=wildcard,
@@ -101,33 +107,27 @@ class Input(wx.Frame):
         dialog.Destroy() 
         self.on_view()
 
-    def printMsg(self, message):
-        print(message == "output")
-        return message
-
     def onRun(self, event):
-        # script name start
-        # dirname = os.path.dirname(__file__)
-        # file = os.path.join(dirname, 'cartoonize.py')
-        # os.system('python3 ' + file)
+        # send message through pubsub
         pub.sendMessage("state", message = 'output')
-        #print(self.pathDir)
-        
+        # run the backend code 
         os.system('python3 driver.py --input ' + self.pathDir + ' --styles ' + self.style)
-        pub.sendMessage("path", message = self.pathDir, arg2 = self.style)
+        # send message through pubsub and close current frame 
+        pub.sendMessage("path", message = self.pathDir, arg2 = self.style)      
         self.frame.Close()
 
 
     def onChoice(self,event): 
-        # self.label.SetLabel("selected "+ self.choice.
-        # GetString( self.choice.GetSelection() ) +" from Choice")
+        # get the style that choosed by the user
         self.style = self.choice.GetString( self.choice.GetSelection())
-        print(self.style)
+
 
     def update_image_on_dnd(self, filepath):
+        # pass filepath -- https://www.blog.pythonlibrary.org/2017/10/25/wx_drag_and_drop_images/
         self.on_view(filepath=filepath)
 
     def on_view(self, filepath=None):
+        # set the image to the bitmap -- https://www.blog.pythonlibrary.org/2017/10/25/wx_drag_and_drop_images/
         if not filepath:
             filepath = self.photoTxt.GetValue()
         
@@ -148,14 +148,12 @@ class Input(wx.Frame):
         self.panel.Refresh()
 
 
-
+# output frame
 class Output(wx.Frame):
     def __init__(self):
+        # create output frame
         wx.Frame.__init__(self)
         self.frame = wx.Frame(None, title='Anima', size = (1600, 800))
-
-
-
         self.panel = wx.Panel(self.frame)
         self.panel.SetBackgroundColour(wx.Colour(233,243,255,255))
 
@@ -163,7 +161,7 @@ class Output(wx.Frame):
         self.frame.Show()
 
     def outputCreateWidgets(self):
-
+        # create components for the output frame
         title = "Anima" 
         lbl = wx.StaticText(self.panel,-1,style = wx.ALIGN_CENTER) 
         font = wx.Font(48, wx.FONTFAMILY_MODERN, wx.NORMAL, wx.NORMAL)
@@ -171,103 +169,51 @@ class Output(wx.Frame):
         lbl.SetFont(font) 
         lbl.SetLabel(title) 
 
-    
-
-        # self.slider = wx.Slider(self.panel, -1, 99, 1, 99, size = (self.img.GetWidth(), 40))
-        # self.Bind(wx.EVT_SLIDER, self.sliderUpdate)
+        # get message from pub
         pub.subscribe(self.getMessage, "path")
-        # self.location = 'input/place.jpg'
-        # self.style = 'shinkai'
 
-
-
-        # imgbef = wx.Image(self.location, wx.BITMAP_TYPE_ANY)
-        # W = imgbef.GetWidth()
-        # H = imgbef.GetHeight()
-        # if W > H:
-        #     NewW = PhotoMaxSize
-        #     NewH = PhotoMaxSize * H / W
-        # else:
-        #     NewH = PhotoMaxSize
-        #     NewW = PhotoMaxSize * W / H
-        # imgbef = imgbef.Scale(NewW,NewH)
-        # self.before = SB.GenStaticBitmap(self.panel, wx.ID_ANY, wx.Bitmap(imgbef), pos = ((1600 - imgbef.GetWidth() * 2)/2, 200))
-        # self.before.SetBitmap(wx.Bitmap(imgbef))
-        # #self.before.SetPosition(100, 100)
-
-        # filename = self.location.split(os.path.sep)[-1]
-        # print(filename)
-        # afterLocation = 'output/'+ self.style + '/adaptive/' + filename
-        # imgAfter = wx.Image(afterLocation, wx.BITMAP_TYPE_ANY)
-        # imgAfter = imgAfter.Scale(NewW, NewH)
-        # self.after = SB.GenStaticBitmap(self.panel, wx.ID_ANY, wx.Bitmap(imgAfter), pos = ((1600 - imgbef.GetWidth() * 2)/2 + imgbef.GetWidth() + 20, 200))
-        # self.after.SetBitmap(wx.Bitmap(imgAfter))
-
+        # create the reset button
         reset = wx.Button(self.panel, label = 'Reset')
         reset.Bind(wx.EVT_BUTTON, self.OnReset)
 
+        # set the layout
         self.mainSizer = wx.BoxSizer(wx.VERTICAL)
-        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
 
         self.mainSizer.Add(lbl, 0, wx.CENTER, 5)
         self.mainSizer.Add(wx.StaticLine(self.panel, wx.ID_ANY),
                            0, wx.ALL|wx.EXPAND, 5)
-
-        #self.sizer.Add(self.before, 0, wx.LEFT|wx.RIGHT, 0)
-        #self.sizer.Add(self.after, 0, wx.LEFT|wx.RIGHT, 0)
-        self.mainSizer.Add(self.sizer, 0, wx.CENTER, 5)
         self.mainSizer.Add(reset, 0, wx.CENTER|wx.TOP, 20)
-        # self.mainSizer.Add(self.slider, 0, wx.CENTER|wx.TOP, 20)
-
-
         self.panel.SetSizer(self.mainSizer)
-        #self.mainSizer.Fit(self.frame)
 
 
         self.panel.Layout()
 
-    # def sliderUpdate(self, event):
-        # pos = self.slider.GetValue()
-        # pub.sendMessage("state", message = 'output')
-        # self.perc = pos /100
-        # print(1)
-        # imgBefore = wx.Image(self.imgLoc, wx.BITMAP_TYPE_ANY)
-        # imgBefore = imgBefore.Resize(size = (int(self.w * self.perc), self.h), pos = (0,0))
-        # self.before = SB.GenStaticBitmap(self.panel, wx.ID_ANY, wx.Bitmap(imgBefore), pos = ((1200 - self.w)/2, 200))
-        # self.before.SetBitmap(wx.Bitmap(imgBefore))
-
-        # imgAfter = wx.Image(self.imgLoc, wx.BITMAP_TYPE_ANY)
-        # imgAfter = imgAfter.Mirror(horizontally=True)
-        # imgAfter = imgAfter.Resize(size = (int(self.w * (1 - self.perc)), self.h), pos = (0,0))
-        # imgAfter = imgAfter.Mirror(horizontally=True)
-        # self.after = SB.GenStaticBitmap(self.panel, wx.ID_ANY, wx.Bitmap(imgAfter), pos = ((1200 - self.w)/2 + int(self.w * self.perc) + 100, 200))
-        # # afterImg = afterImg.Mirror(horizontally=True)
-        # self.after.SetBitmap(wx.Bitmap(imgAfter))
-        # self.sizer.Clear()
-        # self.sizer.Add(self.before, 0, wx.CENTER, 0)
-        # self.sizer.Add(self.after, 0, wx.CENTER, 0)
-        # self.panel.Refresh()
 
     def OnReset(self, event):
+        # the pass the message and close current frame
         pub.sendMessage("state", message = 'input')
         self.frame.Close()
 
     def getMessage(self, message, arg2=None):
+        # get messages from the pub
         self.location = message
         if arg2:
             self.style = arg2
         else:
             self.style = shinkai
 
+        # scale the image, preserving the aspect ratio
         imgbef = wx.Image(self.location, wx.BITMAP_TYPE_ANY)
         W = imgbef.GetWidth()
         H = imgbef.GetHeight()
         if W > H:
-            NewW = PhotoMaxSize
-            NewH = PhotoMaxSize * H / W
+            NewW = maxSize
+            NewH = maxSize * H / W
         else:
-            NewH = PhotoMaxSize
-            NewW = PhotoMaxSize * W / H
+            NewH = maxSize
+            NewW = maxSize * W / H
+
+        # open images that import by user and output by this application
         imgbef = imgbef.Scale(NewW,NewH)
         self.before = SB.GenStaticBitmap(self.panel, wx.ID_ANY, wx.Bitmap(imgbef), pos = ((1600 - imgbef.GetWidth() * 2)/2, 150))
         self.before.SetBitmap(wx.Bitmap(imgbef))
@@ -285,27 +231,22 @@ class Output(wx.Frame):
 
 
 class controller(wx.App):
-
+# controller to control the frame
     def __init__(self):
+        # create the application
         wx.App.__init__(self)
         self.frame = Input()
         pub.subscribe(self.getMSG, "state")
 
-        #self.frame
-        # if pub.subscribe(Input().printMsg, 'state') == "input":
-        #     #frame = Input()
-        #     print(pub.subscribe(Input().printMsg, 'state'))
-        # if pub.subscribe(Input().printMsg, 'state') == "output":
-        #     print(pub.subscribe(Input().printMsg, 'state'))
 	
     def getMSG(self, message):
+        # set frame based on the message
         if message == "input":
             self.frame = Input()
         else:
             self.frame = Output()
 
-
+# create this application and mainloop
 if __name__ == "__main__":
     app = controller()
-
     app.MainLoop()
